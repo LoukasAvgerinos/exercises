1abffb43b289c84c83a6dbea5c0281e26ccfe1af
LucLucifer <flouk1982@hotmail.com>
2025-02-24 13:07:42 +0200

    Text Style

--- lib/views/basic_home.dart
+++ lib/views/basic_home.dart
@@ -7,7 +7,26 @@ class BasicHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Basic Home')),
-      body: Center(child: Text('Hello, World!')),
+      //body: Center(child: Text('Hello, World!')),
+      body: Center(
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.green,
+            color: Colors.white,
+            letterSpacing: 1.5,
+            shadows: [
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.black,
+                offset: Offset(5.0, 5.0),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
